a2cdadb21c881ae0893c7ff07804cf521fe187c8
ryuta-goto <mugi2006@gmail.com>
2025-06-29 13:23:59 +0900

    Add unit test for get_hello_message function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", get_hello_message());
+}
+
+fn get_hello_message() -> String {
+    String::from("Hello, world!")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_hello_message() {
+        assert_eq!(get_hello_message(), "Hello, world!");
+    }
 }
